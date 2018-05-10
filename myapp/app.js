3dced3decb64e9f2e6aaf65f04d7528c583a4f2e
wangpengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2018-05-10 23:23:40 +0800

    Upload a demp about express

---
+++ myapp/app.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello chouyuanyuan')
+})
+
+app.listen(3000);
